b9e2e106a6a1706fa7c0b60451996e37bed148bd
Md. Rafiul Islam <rislamshaon95@gmail.com>
2023-08-19 01:42:03 +0600

    rhombus, pentagon and ellipse buttons are made functionalised

--- script.js
+++ script.js
@@ -18,3 +18,25 @@ document.getElementById('btn-parallelogram').addEventListener('click', function(
     const area = parallelogramArea(b, h);
     setValueById('result-parallelogram', area);
 })
+
+document.getElementById('btn-rhombus').addEventListener('click', function(){
+    const d1 = getInputValueById('d1');
+    const d2 = getInputValueById('d2');
+    const area = rhombusArea(d1, d2);
+    setValueById('result-rhombus', area);
+})
+
+document.getElementById('btn-pentagon').addEventListener('click', function(){
+    const p = getInputValueById('perimeter');
+    const a = getInputValueById('apothem');
+    const area = pentagonArea(p, a);
+    setValueById('result-pentagon', area);
+})
+
+document.getElementById('btn-ellipse').addEventListener('click', function(){
+    const a = getInputValueById('major-axis');
+    const b = getInputValueById('minor-axis');
+    const area = ellipseArea(a, b);
+    setValueById('result-ellipse', area);
+})
+
